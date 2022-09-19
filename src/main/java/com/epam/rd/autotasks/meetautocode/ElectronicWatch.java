bfe21b41dd54f8906c0bb7bdd4a6c1c78db3a0fd
Evgeniy Sharychenkov <sanctio@yandex.ru>
2022-09-19 19:58:08 +0300

    first commit

--- src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
+++ src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
@@ -4,9 +4,17 @@ import java.util.Scanner;
 
 public class ElectronicWatch {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         int seconds = scanner.nextInt();
+        System.out.println(timeToString(seconds));
 
 
+    }
+    public static String timeToString(int secs) {
+        int hour = secs / 3600 % 24,
+                min = secs % 3600 / 60,
+                sec = secs % 3600 % 60;
+        return String.format("%01d:%02d:%02d", hour, min, sec);
     }
 }
